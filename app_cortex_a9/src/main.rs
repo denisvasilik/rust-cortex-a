1a96a55c82513e595b27c4fbc073163e98052a70
Denis Vasilik <contact@denisvasilik.com>
2019-02-09 08:59:25 +0100

    Removed unnecessary comments

--- app_cortex_a9/src/main.rs
+++ app_cortex_a9/src/main.rs
@@ -7,47 +7,38 @@
 extern crate panic_abort;
 
 use cortex_a::{asm, regs::*};
-use cortex_m_semihosting::{hprintln};
+use cortex_m_semihosting::{debug, hprintln};
 
 global_asm!(include_str!("start.S"));
 
 #[no_mangle]
 pub fn entry_point() -> ! {
-    hprintln!("Hello, world!").unwrap();
-    loop {
-        asm::nop();
-        unsafe {
-            asm!("nop" :::: "volatile");
-            asm!("nop" :::: "volatile");
-            asm!("nop" :::: "volatile");
-            asm!("nop" :::: "volatile");
-        }
-        let _stack_pointer = SP.get();
-    }
+    hprintln!("Cortex A9 semihosting example").unwrap();
+
+    let _stack_pointer = SP.get();
+
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
 }
 
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern fn rust_eh_personality() {
-    // Intentionally left blank!!!
 }
 
 #[no_mangle]
 pub extern fn __aeabi_unwind_cpp_pr0() {
-    // Intentionally left blank!!!
 }
 
 #[no_mangle]
 pub extern fn rust_begin_unwind() {
-    // Intentionally left blank!!!
 }
 
 #[no_mangle]
 pub extern fn _Unwind_Resume() {
-
 }
 
 #[no_mangle]
 pub extern fn __aeabi_unwind_cpp_pr1() {
-
 }
